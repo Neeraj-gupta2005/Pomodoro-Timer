8fe6d9aae972e9da7d6a680f68926d9c1a859c01
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-04-04 22:32:02 +0530

    navbar button component added

---
+++ src/components/NavbarButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import DownArrow from './../svg/DownArrow';
+
+function NavbarButton({ text, Icon , isArrow}) {
+  return (
+    <div className="flex items-center gap-2 px-[5px] rounded-md hover:bg-zinc-700 hover:text-zinc-400 cursor-pointer duration-100 transition-all">
+      <div>
+        <Icon className="h-[13px] w-[13px] fill-current" />
+      </div>
+      <p className="text-[14px] font-sans">{text}</p>
+      <div>
+        {isArrow && <DownArrow />}
+      </div>
+    </div>
+  );
+}
+
+export default NavbarButton;
--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import NavbarButton from './NavbarButton'
+import FolderIcon from '../svg/Foldericon'
+import GeneralSVG from '../svg/GeneralSVG'
 
 function Pomodoro() {
   return (
     <div className='flex flex-1 flex-col h-full'>
-      
-      <div className='h-[33px] w-full bg-zinc-900 border-b border-b-zinc-800'>
-        
+      <div className='h-[33px] flex items-center text-center px-2 w-full bg-zinc-900 border-b border-b-zinc-800'>
+        <div className='flex gap-2'>
+          <p className='text-[14px] font-sans p-0.5'>Pomodoro</p>
+          <p className='text-[14px] text-zinc-700 mr-2 font-sans'>/</p>
+          <NavbarButton text={"Personal"} Icon={FolderIcon} isArrow={true}/>
+          <NavbarButton text={"General"} Icon={GeneralSVG} isArrow={true}/>
+        </div>
       </div>
+
       <div className='flex flex-1 flex-grow flex-col lg:flex-row gap-3 p-3'>
         <div className='rounded-2xl w-[498px]  bg-zinc-900 border border-zinc-800 p-3'>h</div>
         <div className='rounded-2xl flex-1 bg-zinc-900 border border-zinc-800 p-3'>h</div>
