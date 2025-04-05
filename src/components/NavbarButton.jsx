4eaccdacb9abec6020accf16032a0ffa54000f23
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-04-05 14:05:36 +0530

    navbar complete

--- src/components/NavbarButton.jsx
+++ src/components/NavbarButton.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import DownArrow from './../svg/DownArrow';
 
-function NavbarButton({ text, Icon , isArrow}) {
+function NavbarButton({ text, Icon , isArrow, classes}) {
   return (
-    <div className="flex items-center gap-2 px-[5px] rounded-md hover:bg-zinc-700 hover:text-zinc-400 cursor-pointer duration-100 transition-all">
+    <div className={classes ? classes : "flex items-center gap-2 px-[5px] rounded-md hover:bg-zinc-700 hover:text-zinc-400 cursor-pointer duration-100 transition-all"}>
       <div>
         <Icon className="h-[13px] w-[13px] fill-current" />
       </div>
--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -1,26 +1,65 @@
-import React from 'react'
-import NavbarButton from './NavbarButton'
-import FolderIcon from '../svg/Foldericon'
-import GeneralSVG from '../svg/GeneralSVG'
+import React from "react";
+import NavbarButton from "./NavbarButton";
+import FolderIcon from "../svg/Foldericon";
+import GeneralSVG from "../svg/GeneralSVG";
+import FeedbackSVG from "./../svg/FeedbackSVG";
+import FocusSVG from "./../svg/FocusSVG";
 
 function Pomodoro() {
   return (
-    <div className='flex flex-1 flex-col h-full'>
-      <div className='h-[33px] flex items-center text-center px-2 w-full bg-zinc-900 border-b border-b-zinc-800'>
-        <div className='flex gap-2'>
-          <p className='text-[14px] font-sans p-0.5'>Pomodoro</p>
-          <p className='text-[14px] text-zinc-700 mr-2 font-sans'>/</p>
-          <NavbarButton text={"Personal"} Icon={FolderIcon} isArrow={true}/>
-          <NavbarButton text={"General"} Icon={GeneralSVG} isArrow={true}/>
+    <div className="flex flex-1 flex-col h-full">
+      {/* navbar */}
+      <div className="h-[33px] justify-between flex items-center text-center pl-2 w-full bg-zinc-900 border-b border-b-zinc-800">
+        <div className="flex gap-2">
+          <p className="text-[14px] font-sans p-0.5">Pomodoro</p>
+          <p className="text-[14px] text-zinc-700 mr-2 font-sans">/</p>
+          <NavbarButton text={"Personal"} Icon={FolderIcon} isArrow={true} />
+          <NavbarButton text={"General"} Icon={GeneralSVG} isArrow={true} />
+        </div>
+        <div className="flex h-full gap-2">
+          <div className="flex h-full ">
+            <NavbarButton
+              text={"Feedback"}
+              Icon={FeedbackSVG}
+              isArrow={false}
+              classes={
+                "h-full px-1 flex items-center gap-1 cursor-pointer hover:bg-zinc-800 "
+              }
+            />
+            <NavbarButton
+              text={"Focus"}
+              Icon={FocusSVG}
+              isArrow={false}
+              classes={
+                "h-full px-1 flex items-center gap-1 cursor-pointer hover:bg-zinc-800 "
+              }
+            />
+          </div>
+          <div className="flex h-full">
+            <div className="h-full px-1 flex items-center gap-1 text-[14px]">
+              <span className="text-zinc-50 ">11</span>
+              <span>:</span>
+              <span className="text-zinc-50 ">20</span>
+              <span>AM</span>
+            </div>
+            <div className="h-full bg-zinc-800 text-zinc-400 px-2 flex items-center gap-1 text-[14px] font-bold  cursor-pointer hover:bg-zinc-700 ml-1 transition duration-100">
+              <span>12</span>
+              <span>hr</span>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className='flex flex-1 flex-grow flex-col lg:flex-row gap-3 p-3'>
-        <div className='rounded-2xl w-[498px]  bg-zinc-900 border border-zinc-800 p-3'>h</div>
-        <div className='rounded-2xl flex-1 bg-zinc-900 border border-zinc-800 p-3'>h</div>
+      <div className="flex flex-1 flex-grow flex-col lg:flex-row gap-3 p-3">
+        <div className="rounded-2xl w-[498px]  bg-zinc-900 border border-zinc-800 p-3">
+          h
+        </div>
+        <div className="rounded-2xl flex-1 bg-zinc-900 border border-zinc-800 p-3">
+          h
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Pomodoro
+export default Pomodoro;
